de97245333f525e7456f7a7c83116115fb8b6488
reshetivskyy <renat.reshetivskyy@gmail.com>
2025-08-07 13:22:55 +0200

    bugfix

--- db/session.py
+++ db/session.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///bot.db"
-engine = create_async_engine(DATABASE_URL, echo=False)
+from config import DB_URL
+
+engine = create_async_engine(DB_URL, echo=False)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
